000e4b31d760e5c76291ea2b86e6162e09f2572a
Francesco <franceso@gmail.com>
2025-12-12 20:50:44 +0100

    first version

--- gitgame.py
+++ gitgame.py
@@ -1,2 +1,42 @@
-# This is my git game
+# This is my git game a tic tac toe my friend
+
+board = [
+[' ', ' ', ' '],
+[' ', ' ', ' '],
+[' ', ' ', ' ']
+]
+
+def display_board():
+    for row in board:
+        print(' | '.join(row))
+        print('---------')
+
+def get_move():
+    move = input('Enter Move: ').split()
+    row = int(move[0])
+    col = int(move[1])
+    return row, col
+
+
+
+player = True
+
+display_board()
+
+while True:
+    r, c =  get_move()
+
+    board[r-1][c-1] = 'X' if player else 'O'
+
+
+    display_board() 
+
+    player = not player
+
+
+
+
+
+
+
 
